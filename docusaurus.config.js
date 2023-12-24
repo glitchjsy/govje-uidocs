c47684028a03fa33bd9e3b8ec2596f0873633d30
Luke <luke.wyatt21@gmail.com>
2023-12-24 20:37:38 +0000

    Update edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           routeBasePath: "/",
-          editUrl: "https://github.com/glitchjsy/uidocs"
+          editUrl: "https://github.com/glitchjsy/govje-uidocs"
         },
         blog: false,
         theme: {
